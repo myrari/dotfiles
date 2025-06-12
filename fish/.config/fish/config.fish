6dc75dc68b104ca922882f2219f4d43ba3413a0a
Myra Roberts <myra@myrari.net>
2025-06-12 13:15:57 -0700

    More fish changes bc I can't decide on colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,32 +2,50 @@
 set -g fish_greeting ""
 
 # set editor variable
-set -gx EDITOR "nvim"
+set -gx EDITOR nvim
 
 # easy config reload
 function reload
-	source "$HOME/.config/fish/config.fish"
+    source "$HOME/.config/fish/config.fish"
 end
 
 # short ls alias
 function l
-	ls -la $argv
+    ls -la $argv
 end
 
 # add cargo bin
 if test -e "$HOME/.cargo"
-	fish_add_path "$HOME/.cargo/bin/"
+    fish_add_path "$HOME/.cargo/bin/"
 end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-	# A way to reload the shell à la "zsh"
-	function fish
-		source ~/.config/fish/config.fish
-	end
+    # A way to reload the shell à la "zsh"
+    function fish
+        source ~/.config/fish/config.fish
+    end
 
-	# use starship
-	starship init fish | source
+	# oendark theme
+    if set -q VIM
+        # Using from vim/neovim.
+        set onedark_options -256
+    else if string match -iq "eterm*" $TERM
+        # Using from emacs.
+        function fish_title
+            true
+        end
+        set onedark_options -256
+    end
+
+	# custom red colors w/ more saturation
+	set_onedark_color red D04134 current
+	set_onedark_color brred FB505E current
+
+    set_onedark $onedark_options
+
+    # use starship
+    starship init fish | source
 
 end
