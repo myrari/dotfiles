46fcc27c3ee7b3adc50e3c9571fcddd5a3ee1ec6
Myra Roberts <myra@myrari.net>
2025-07-11 12:55:57 -0700

    Added manual add for fish config
    It just randomly stopped working for some reason and now I need this
    extra thing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,6 +9,16 @@ function reload
     source "$HOME/.config/fish/config.fish"
 end
 
+# homebrew
+if test -e /opt/homebrew/bin
+	set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+	set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+	set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+	fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+	if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+	if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
+end
+
 # short alias to eza (or just ls)
 function l
     if type -q eza
